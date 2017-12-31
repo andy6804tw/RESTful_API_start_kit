ae556d77f9bdcba5bb1b991f1c26b243907b90a8
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-12-31 11:49:46 +0800

    done Part7

--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import mysql from 'mysql';
+// Router
+import article from './article.route';
 
 import config from './../../config/config';
 
@@ -11,24 +12,8 @@ router.get('/', (req, res) => {
   res.send(`此路徑是: localhost:${config.port}/api`);
 });
 
-/* mysql連線測試 */
-router.get('/sqlTest', (req, res) => {
-  const connectionPool = mysql.createPool({
-    connectionLimit: 10,
-    host: config.mysqlHost,
-    user: config.mysqlUserName,
-    password: config.mysqlPass,
-    database: config.mysqlDatabase
-  });
-  connectionPool.getConnection((err, connection) => {
-    if (err) {
-      res.send(err);
-      console.log('連線失敗！');
-    } else {
-      res.send('連線成功！');
-      console.log(connection);
-    }
-  });
-});
+/** Article Router */
+router.use('/article', article);
+
 
 export default router;
