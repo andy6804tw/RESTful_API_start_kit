184244748db708ca630d732783ec00a1796822e8
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-01-01 11:42:18 +0800

    done Part10-User-api(2)

--- src/server/routes/user.route.js
+++ src/server/routes/user.route.js
@@ -7,5 +7,9 @@ router.route('/')
   .get(userCtrl.userGet) /** 取得 User 所有值組 */
   .post(userCtrl.userPost); /** 新增 User 值組 */
 
+router.route('/:user_id')
+  .put(userCtrl.userPut) /** 修改 User 值組 */
+  .delete(userCtrl.userDelete); /** 刪除 User 值組 */
+
 
 export default router;
